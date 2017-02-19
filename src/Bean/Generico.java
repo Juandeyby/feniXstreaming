ae09982955803f015c5a8a2324a734b2930da052
Cesar Cuarite <csssssr39@gmail.com>
2017-02-19 02:57:07 -0500

    commit

--- src/Bean/Generico.java
+++ src/Bean/Generico.java
@@ -9,6 +9,7 @@ import Local.Conexion.Mysql;
 import Utilitarios.MiModel;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import javax.swing.JTable;
@@ -40,21 +41,36 @@ public class Generico {
                  }
                  i++;
             }
-            if (i==0)return;
-            String [][] data= new  String [arr.size()][arr.get(0).size()]; 
-            for (int j = 0; j <arr.size(); j++) {
-                      for (int j1 = 0; j1 <arr.get(0).size(); j1++) {
-                     data[j][j1]=arr.get(j).get(j1);
-                 }
-                 }
+            if (i == 0) {
+                return;
+            }
+            String[][] data = new String[arr.size()][arr.get(0).size()];
+            for (int j = 0; j < arr.size(); j++) {
+                for (int j1 = 0; j1 < arr.get(0).size(); j1++) {
+                    data[j][j1] = arr.get(j).get(j1);
+                }
+            }
             MiModel model = new MiModel(data, cabecera);
 
             JTabla.setModel(model);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+    }
+
+    public static ArrayList<String> consultaCon(String nomTabla, String atrBuscar, String where) throws SQLException {
+        ArrayList<String> desu = new ArrayList<String>();
+        Statement sent2 = cn.createStatement();
+        String sql_cons = "select * from " + nomTabla + " " + where;
+        System.out.println(sql_cons);
+        ResultSet rs2 = sent2.executeQuery(sql_cons);
+
+        while (rs2.next()) {
+            desu.add(rs2.getString(atrBuscar));
+        }
+        return desu;
+    }
     
-    
-       }
     
 }
